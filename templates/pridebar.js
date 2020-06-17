852c1c2c97cc3384608bb3564f018947730852ce
Valentina Herrero <60630391+valentinaherrero@users.noreply.github.com>
2020-06-17 16:01:29 -0400

    updated bubblechart

--- templates/pridebar.js
+++ templates/pridebar.js
@@ -9,10 +9,113 @@ d3.json("http://pridetrip.herokuapp.com/pridedata_avg").then(payment=>{
 
     // Build a Bubble Chart
     var bubbleLayout = {
-      margin: { t: 0 },
+      title: {
+        text: "Pride Week",
+        font: {
+          color: "#fff",
+        },
+      },
+      paper_bgcolor: "#2b3e50",
+      plot_bgcolor: "#2b3e50",
+      showlegend: false,
+      autosize: false,
+      width: 900,
+      height: 600,
       hovermode: "closest",
-      xaxis: { title: "OTU ID" },
-      margin: { t: 100}
+      xaxis: { 
+        title: {
+          text: "Drop Off Zone",
+          font: {
+          color: "#fff",
+          },
+          },
+        automargin: true,
+        showgrid: false,
+        color: "#fff",
+      },
+      yaxis: { 
+        title: {
+          text: "Tip Average",
+          font: {
+          color: "#fff",
+          },
+          },
+        automargin: true,
+        showgrid: false,
+        showline: true,
+        color: "#fff",
+      },
+    };
+
+    // var bubbleLayout(automargin=True);
+
+    var bubbleData = [
+      {
+        x: location,
+        y: avgTip,
+          text: location,
+        mode: "markers",
+        marker: {
+          size: avgTip,
+          colorscale: "Earth"
+        }
+      }
+    ];
+
+    Plotly.newPlot("pridewk", bubbleData, bubbleLayout, {scrollZoom: true});
+
+  })
+
+
+
+
+  d3.json("http://pridetrip.herokuapp.com/priorwkdata_avg").then(payment=>{
+    var test = payment.results
+    console.log(test)
+    var location = test.map(x => x.dozone);
+    console.log(location)
+
+    var avgTip = test.map(x => x.avg_tip_amount)
+    console.log(avgTip)
+
+    // Build a Bubble Chart
+    var bubbleLayout = {
+      title: {
+        text: "Prior Week",
+        font: {
+          color: "#fff",
+        },
+      },
+      paper_bgcolor: "#2b3e50",
+      plot_bgcolor: "#2b3e50",
+      showlegend: false,
+      autosize: false,
+      width: 900,
+      height: 600,
+      hovermode: "closest",
+      xaxis: { 
+        title: {
+          text: "Drop Off Zone",
+          font: {
+          color: "#fff",
+          },
+          },
+        automargin: true,
+        showgrid: false,
+        color: "#fff",
+      },
+      yaxis: { 
+        title: {
+          text: "Tip Average",
+          font: {
+          color: "#fff",
+          },
+          },
+        automargin: true,
+        showgrid: false,
+        showline: true,
+        color: "#fff",
+      },
     };
     var bubbleData = [
       {
@@ -28,48 +131,8 @@ d3.json("http://pridetrip.herokuapp.com/pridedata_avg").then(payment=>{
       }
     ];
 
-    Plotly.newPlot("svg-area", bubbleData, bubbleLayout);
+    Plotly.newPlot("priorwk", bubbleData, bubbleLayout);
 
   })
-
-
-
-
-// // Dataset we will be using to set the height of our rectangles.
-// console.log(avgTip);
-
-// // Setting the dimensions for the SVG container
-// var svgHeight = 1000;
-// var svgWidth = 1000;
-
-// var svg = d3
-//   .select("#svg-area")
-//   .append("svg")
-//   .attr("height", svgHeight)
-//   .attr("width", svgWidth);
-
-// // svgGroup now refers to the `g` (group) element appended.
-// // The SVG group would normally be aligned to the top left edge of the chart.
-// // Now it is offset to the right and down using the transform attribute
-// var svgGroup = svg.append("g")
-//   .attr("transform", "translate(50, 100)");
-
-// // Selects all rectangles currently on the page - which is none - and binds our dataset to them. If there are no rectangles, it will append one for each piece of data.
-// svgGroup.selectAll("rect")
-//   .data(avgTip)
-//   .enter()
-//   .append("rect")
-//   .attr("width", 5)
-//   .attr("height", function(data) {
-//     return data * 100;
-//   })
-//   .attr("x", function(data, index) {
-//     return index * 6;
-//   })
-//   .attr("y", function(data) {
-//     return 600 - data * 100;
-//   })
-//   .attr("class", "bar");
-  
 
 
