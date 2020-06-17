06032c2e1407725d4246a05aeca2817250835b6e
RW <yingyingwang0312@gmail.com>
2020-06-17 13:39:06 -0400

    dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -10,66 +10,42 @@ function populateDropDown() {
 })}
 
 // want to take average based on drop off zone location
-// do I need to map?
-// do I need to write query in app.py file?
-// Which is the correct https link?
-// how can I make a legend for payment type?
+// do I need to map? object.value
 // How is this code picking up the users dropdown selection?
 
 
-function populateTable() {
+function populateTable(dozone) {
     var pannel = d3.select("#trip-metadata")
-    var payment_type = []
-    var trip_distance = []
-    var fare_amount = []
-    var tip_amount = []
-    var passenger_count = []
-    fetch('https://pridetrip.herokuapp.com/pridedata?length=100').then(data=>data.json()).then(d=>{
+    document.getElementById("trip-metadata").innerHTML = ""
+    passenger_count = []
+
+    fetch('https://pridetrip.herokuapp.com/pridedata?length=10000').then(data=>data.json()).then(d=>{
     d.results.forEach(r=>{
-        console.log(r);
-//         table_data = document.createElement("td")
-        // "Number of Passengers:" = r.passenger_count
-        // "Fare:" = r.fare_amount
-        // "Tip:" = r.tip_amount
-        // "Payment Type:" = r.payment_type
-        // "Distance:" = r.trip_distance  
-//         document.getElementById('trip-metadata').add(table_data)
-    
+        if (dozone == r.dozone || typeof dozone == "undefined") {
+            passenger_count.push(r.passenger_count)
+        }
+       
+    })
+    passenger = jStat.mode(passenger_count)
+    pannel.append("h6").text("Passenger Count: " + passenger)   
 
-    pannel.append("h6").text("Payment Type: " + avg_payment_type)
-    pannel.append("h6").text("Payment Type: " + avg_payment_type)
-
-})}
+ 
 
 
+})
+}
+// pannel.on("change", populateTable)   
 
-
-populateDropDown();
-
-populateTable();
+populateTable()
+populateDropDown()
+callChart()
 
 
 
 
-// // Match name of function to Valentina's function to call graphs (Sankey and Payment/Tips)
-// function populateGraphs() {
-//     fetch('/priorwkdata').then(data=>data.json()).then(d=>{
-//     d.datasets.forEach(n=>{
-//         o = document.createElement("option")
-//         o.text = n
-//         document.getElementById('dataSelect').add(o)
-//     })
-//     updateData()
-// })
-// }
+function optionChanged(dropoffzone) {
+    console.log(dropoffzone);
+    populateTable(dropoffzone)
+    callChart(dropoffzone)
 
-
-// Promise needs to initiate the populateDropDown, graphs and table
-// fetch('/pridedata').then(data=>data.json()).then(d=>{
-//     d.charts.forEach(n=>{
-//         o = document.createElement("option")
-//         o.text = n
-//         document.getElementById('chartSelect').add(o)
-//     })
-//     dataEndpoints()
-// })
+}
